7d5799fad19ba7a0a9c1638d77e43961ba40bebd
Damien PLENARD <dplenard@vente-privee.com>
2018-11-27 16:18:05 +0100

    create security group for web servers

--- main.tf
+++ main.tf
@@ -53,3 +53,30 @@ resource "aws_security_group" "wp_lb_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "wp_web_sg" {
+    name = "wp-web"
+    description = "Security Group for Wordpress web server"
+    vpc_id = "${aws_vpc.main.id}"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
