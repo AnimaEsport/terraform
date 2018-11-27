3e1da68dfed26ec48c53700eac0e0b47e52d14a8
Damien PLENARD <dplenard@vente-privee.com>
2018-11-27 16:16:20 +0100

    create 2 public subnet for eu-west-3a & eu-west-3b zones

--- main.tf
+++ main.tf
@@ -10,3 +10,26 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
     vpc_id = "${aws_vpc.main.id}"
 }
+
+resource "aws_subnet" "public" {
+    vpc_id                  = "${aws_vpc.main.id}"
+    count                   = 2
+    cidr_block              = "${element(split(",", "172.16.10.0/24,172.16.20.0/24"), count.index)}"
+    availability_zone       = "${element(split(",", "eu-west-3a,eu-west-3b"), count.index)}"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main.id}"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count          = 2
+    subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id = "${aws_route_table.public.id}"
+}
