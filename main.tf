2ac5356bd0ca208dfd3a7b1ebe4f931ea1cb4905
Damien PLENARD <dplenard@vente-privee.com>
2018-11-27 16:15:27 +0100

    create main AWS VPC and Internet Gateway

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region = "eu-west-3"
 }
+
+resource "aws_vpc" "main" {
+    cidr_block           = "172.16.0.0/16"
+    enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+}
